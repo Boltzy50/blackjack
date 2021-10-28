f6c110ec446eb78b5cb06db6eaa22126b64a42b2
Jeff <jcboltz50@gmail.com>
2021-10-28 19:31:34 -0400

    Second working commit

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,10 @@
 window.addEventListener('DOMContentLoaded', function() {
-  // Execute after page load
-})
+  const dealBtn = document.querySelector("#deal-button");
+    // Add a click event listener to the button
+    dealBtn.addEventListener('click', function() {
+      alert("hello world")
+
+      const newCard = document.createElement('img');
+      console.log(newCard);
+    })
+  })
